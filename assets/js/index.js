16ebd479529168350c0b974410634e2ece0babba
liuyuxibuyu <1491156161@qq.com>
2020-09-16 08:58:03 +0800

    完成了index主页的功能

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,87 +1,130 @@
-$(function () {
-  $('.text-avatar').hide()
-  $('.layui-nav-img').hide()
-  getUserInfo();
+// $(function() {
+//   // 调用 getUserInfo 获取用户基本信息
+//   getUserInfo()
 
-  // 获取弹出层对象
-  var layer = layui.layer;
+//   var layer = layui.layer
 
-  // 点击按钮，实现退出功能
-  $('#btnLogout').on('click', function() {
-    // 提示用户是否确认退出
-    layer.confirm('确定退出登录?', { icon: 3, title: '提示' }, function(index) {
-      //do something
-      // 1. 清空本地存储中的 token
-      localStorage.removeItem('token')
-      // 2. 重新跳转到登录页面
-      location.href = '/login.html'
+//   // 点击按钮，实现退出功能
+//   $('#btnLogout').on('click', function() {
+//     console.log(123);
+//     // 提示用户是否确认退出
+//     layer.confirm('确定退出登录?', { icon: 3, title: '提示' }, function(index) {
+//       //do something
+//       // 1. 清空本地存储中的 token
+//       localStorage.removeItem('token')
+//       // 2. 重新跳转到登录页面
+//       location.href = './login.html'
 
-      // 关闭 confirm 询问框
-      layer.close(index)
-    })
-  })
+//       // 关闭 confirm 询问框
+//       layer.close(index)
+//     })
+//   })
+// })
+
+// // 获取用户的基本信息
+// function getUserInfo() {
+//   $.ajax({
+//     method: 'GET',
+//     url: '/my/userinfo',
+//     success: function(res) {
+//       if (res.status !== 0) {
+//         return layui.layer.msg('获取用户信息失败！')
+//       }
+//       // 调用 renderAvatar 渲染用户的头像
+//       renderAvatar(res.data)
+//     }
+//     // 不论成功还是失败，最终都会调用 complete 回调函数
+//     // complete: function(res) {
+//     //   // console.log('执行了 complete 回调：')
+//     //   // console.log(res)
+//     //   // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
+//     //   if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+//     //     // 1. 强制清空 token
+//     //     localStorage.removeItem('token')
+//     //     // 2. 强制跳转到登录页面
+//     //     location.href = '/login.html'
+//     //   }
+//     // }
+//   })
+// }
+
+// // 渲染用户的头像
+// function renderAvatar(user) {
+//   // 1. 获取用户的名称
+//   var name = user.nickname || user.username
+//   // 2. 设置欢迎的文本
+//   $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
+//   // 3. 按需渲染用户的头像
+//   if (user.user_pic !== null) {
+//     // 3.1 渲染图片头像
+//     $('.layui-nav-img')
+//       .attr('src', user.user_pic)
+//       .show()
+//     $('.text-avatar').hide()
+//   } else {
+//     // 3.2 渲染文本头像
+//     $('.layui-nav-img').hide()
+//     var first = name[0].toUpperCase()
+//     $('.text-avatar')
+//       .html(first)
+//       .show()
+//   }
+// }
+
+
+/////////////////////////////////////////////////////////////////////////////
+$(function() {
+//  调用 getUserInfo 获取用户基本信息
+ getUserInfo();
+ var layer = layui.layer
+ // 点击按钮，实现退出功能
+ $('#btnLogout').on('click',function(){
+  //  console.log(123);
+  layer.confirm('确定退出吗?', {icon: 3, title:'提示'}, function(index){
+    //do something
+    // 清空token
+    localStorage.removeItem('token');
+    // 跳转到login
+    location.href = './login.html'
+    layer.close(index);
+  });
+ })
 })
-
 // 获取用户的基本信息
 function getUserInfo() {
   $.ajax({
-    method: 'GET',
-    url: '/my/userinfo',
-    // headers 就是请求头配置对象
-    headers: {
-      Authorization: localStorage.getItem('token') || ''
-    },
-    success: function(res) {
+    method : 'GET',
+    url : '/my/userinfo',
+    success : function(res) {
+      console.log(res);
       if (res.status !== 0) {
         return layui.layer.msg('获取用户信息失败！')
       }
       // 调用 renderAvatar 渲染用户的头像
       renderAvatar(res.data)
-    },
-    // // 不论成功还是失败，最终都会调用 complete 回调函数
-    // complete: function(res) {
-    //   // console.log('执行了 complete 回调：')
-    //   // console.log(res)
-    //   // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
-    //   if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
-    //     // 1. 强制清空 token
-    //     localStorage.removeItem('token')
-    //     // 2. 强制跳转到登录页面
-    //     location.href = '/login.html'
-    //   }
-    // }
+    }
   })
 }
 
-// 渲染用户的头像
-function renderAvatar(user) {
-  // 1. 获取用户的名称
-  var name = user.nickname || user.username
-  // 2. 设置欢迎的文本
-  $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
-  // 3. 按需渲染用户的头像
-  if (user.user_pic !== null) {
-    // 3.1 渲染图片头像
-    $('.layui-nav-img')
-      .attr('src', user.user_pic)
-      .show()
-    $('.text-avatar').hide()
-  } else {
-    // 3.2 渲染文本头像
-    $('.layui-nav-img').hide()
-    var first = name[0].toUpperCase()
-    $('.text-avatar')
-      .html(first)
-      .show()
-  }
-}
 
-  /**
-   * 主动切换 nav 
-   * @param {*} newNavId 新的 nav 元素 id
-   * @param {*} oldNavId 旧的 nav 元素 id
-   */
-  function changeNav (newNavId, oldNavId) {
-    $('#' + oldNavId).removeClass('layui-this');
-    $('#' + newNavId).addClass('layui-this');
+// 渲染用户头像
+function  renderAvatar(user) {
+  // 获取用户名
+  var name  = user.nickname || user.username;
+  // 把用户名渲染到页面
+  $('#welcome').html('欢迎 &nbsp'+ name);
+  // 判断是否有头像
+  if(user.user_pic != null) {
+    // 如果有头像就让获取头像路径并显示
+    $('.layui-nav-img').attr('src',user.user_pic).show();
+    // 让文字头像隐藏
+    $('.text-avatar').hide();
+  }else{
+    // 如果没有头像就让头像隐藏
+    $('.layui-nav-img').hide();
+    // 得到用户名的第一个字符 如果是英文转换为大写
+    var first = name[0].toUpperCase();
+    // 把的到的字符填充到文字图片里面并让他显示
+    $('.text-avatar').html(first).show();
   }
+} 
